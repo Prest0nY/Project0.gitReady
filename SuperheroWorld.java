e2a04b0047def12c3ab45066090787b0ffb30011
Prest0nY <75341414+Prest0nY@users.noreply.github.com>
2025-08-30 18:02:23 -0700

    Created user input for first superhero name

--- SuperheroWorld.java
+++ SuperheroWorld.java
@@ -4,13 +4,16 @@ public class SuperheroWorld
 {
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a superhero name: ");
+        String superheroName1 = scanner.nextLine();
+
 
         Superhero hero1 = new Superhero();
         Superhero hero2 = new Superhero();
 
 
-        hero1.setName("Batman");
+        hero1.setName(superheroName1);
         hero1.setSuperpower("Rich");
         hero1.setCatchPhrase("I'm Batman");
         System.out.println("Name: " + hero1.getName());
